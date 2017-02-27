4b5bcb140608dada6415e2684621983b9e491eef
Niklas Saers <niklas@saers.com>
2017-02-27 22:21:26 +0100

    PullAll test

--- Tests/bolt-swiftTests/bolt_swiftTests.swift
+++ Tests/bolt-swiftTests/bolt_swiftTests.swift
@@ -6,32 +6,66 @@ import packstream_swift
 class bolt_swiftTests: XCTestCase {
     func testConnection() throws {
         let connectionExp = expectation(description: "Login successful")
-        let cypherExp = expectation(description: "Perform cypher query")
         
         let settings = ConnectionSettings(username: "neo4j", password: "<passcode>", userAgent: "Bah")
         let conn = SwiftSocketConnection(hostname: "localhost", settings: settings)
-        try conn.connect() { (success) in
-            if success == true {
-                connectionExp.fulfill()
-                let statement = "CREATE (n:FirstNode {name:{name}}) RETURN n"
-                let parameters = Map(dictionary: [ "name": "Steven" ])
-                let request = Message.run(statement: statement, parameters: parameters)
-                do {
-                    try conn.request(request) { success in
-                        if success {
-                            cypherExp.fulfill()
-                        }
-                    }
-                } catch {
-                    // poop
+        try conn.connect { (success) in
+            do {
+                if success == true {
+                    connectionExp.fulfill()
+                    try self.createNode(connection: conn)
                 }
-                
+            } catch {
+                // poop
             }
         }
-        
+    
         self.waitForExpectations(timeout: 300000) { (error) in
             print("Done")
         }
+
+    }
+    
+    func createNode(connection conn: SwiftSocketConnection) throws -> XCTestExpectation {
+    
+        let cypherExp = expectation(description: "Perform cypher query")
+        
+        let statement = "CREATE (n:FirstNode {name:{name}}) RETURN n"
+        let parameters = Map(dictionary: [ "name": "Steven" ])
+        let request = Message.run(statement: statement, parameters: parameters)
+        try conn.request(request) { success in
+            do {
+                if success {
+                    cypherExp.fulfill()
+                    try self.pullResults(connection: conn)
+                }
+            } catch {
+                // poop
+            }
+        }
+        
+        return cypherExp
+    }
+    
+    func pullResults(connection conn: SwiftSocketConnection) throws -> XCTestExpectation {
+        
+        let pullAllExp = expectation(description: "Perform pull All")
+        
+        let request = Message.pullAll()
+        print("pull all")
+        try conn.request(request) { success in
+            print("got result \(success)")
+            do {
+                if success {
+                    pullAllExp.fulfill()
+                    try self.pullResults(connection: conn)
+                }
+            } catch {
+                // poop
+            }
+        }
+        
+        return pullAllExp
     }
 
 
@@ -63,5 +97,14 @@ class bolt_swiftTests: XCTestCase {
         // result_available_after: 2 (ms)
         // fields: ["n"]
 
+    }
+    
+    func testPullAll() throws {
+        let bytes: [Byte] = [0xb1, 0x71, 0x91, 0xb3, 0x4e, 0x12, 0x91, 0x89, 0x46, 0x69, 0x72, 0x73, 0x74, 0x4e, 0x6f, 0x64, 0x65, 0xa1, 0x84, 0x6e, 0x61, 0x6d, 0x65, 0x86, 0x53, 0x74, 0x65, 0x76, 0x65, 0x6e]
+        let response = try Response.unpack(bytes)
+        
+        // Expected: Record with one Node (ID 18)
+        // label: FirstNode
+        // props: "name" = "Steven"
     }
 }
