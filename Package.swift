3ed89be30202449aaf15229e7b9020355acc3395
Niklas Saers <niklas@saers.com>
2017-05-31 21:11:56 +0200

    Only include ShellOut on macOS and Linux builds

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 import PackageDescription
 
+#if os(macOS) || os(Linux)
 let package = Package(
     name: "Bolt",
 	dependencies: [
@@ -9,3 +10,13 @@ let package = Package(
 		.Package(url: "https://github.com/JohnSundell/ShellOut.git", majorVersion: 1)
 	    ]
 )
+#else
+let package = Package(
+    name: "Bolt",
+	dependencies: [
+	    .Package(url: "https://github.com/niklassaers/PackStream-swift.git",
+	                 majorVersion: 0),
+		.Package(url: "https://github.com/IBM-Swift/BlueSSLService.git", majorVersion: 0),
+	    ]
+)
+#endif
