598414e8685e5ad0b587b87ed5c3fa0681a4e351
Niklas Saers <niklas@saers.com>
2017-11-03 22:01:45 +0100

    Update useragent

--- Sources/ConnectionSettings.swift
+++ Sources/ConnectionSettings.swift
@@ -5,7 +5,7 @@ public struct ConnectionSettings {
     let password: String
     let userAgent: String
 
-    public init(username: String = "neo4j", password: String = "neo4j", userAgent: String = "Bolt-Swift/0.0.1") {
+    public init(username: String = "neo4j", password: String = "neo4j", userAgent: String = "Bolt-Swift/0.9.5") {
 
         self.username = username
         self.password = password
